349ca23963e0c91beea46dfb1342407abcae1424
bzt87 <storiz87@live.se>
2013-06-03 21:42:53 +0200

    first step for coremod

---
+++ common/bzt/RZ/Red_Zone.java
@@ -0,0 +1,10 @@
+package bzt.RZ;
+
+import bzt.RZ.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class Red_Zone {
+
+}
---
+++ common/bzt/RZ/lib/Reference.java
@@ -0,0 +1,8 @@
+package bzt.RZ.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "RZ";
+    public static final String MOD_NAME = "Red-Zone";
+    public static final String VERSION = "0.0.1";
+}
