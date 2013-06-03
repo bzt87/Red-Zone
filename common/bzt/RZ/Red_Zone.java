3f70b6d121119e80f15e5759ceb10b710d418233
bzt87 <storiz87@live.se>
2013-06-03 22:42:53 +0200

    start of the coremod

--- common/bzt/RZ/Red_Zone.java
+++ common/bzt/RZ/Red_Zone.java
@@ -2,9 +2,30 @@ package bzt.RZ;
 
 import bzt.RZ.lib.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
 @Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class Red_Zone {
 
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void load(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+    
 }
